ecba6f282a39f9a949f6637b28aa2c59e331b3b0
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-05-15 23:49:44 +0800

    新增EventChannel

---
+++ Runtime/EventBus/EventChannel.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using Zero53.EventBus;
+using Zero53.Singletons;
+using Object = UnityEngine.Object;
+
+namespace Endfield.EventSystem
+{
+    [CreateAssetMenu(menuName = "Zero53/EventChannel", fileName = "New EventChannel")]
+    public sealed class EventChannel : ScriptableObject
+    {
+#if UNITY_EDITOR
+
+        [SerializeField, TextArea]
+        private string comment;
+        
+#endif
+        public void Subscribe<TEvent>(EventHandler<TEvent> handler)
+        {
+            Singleton<EventBus<EventChannelPublisher, TEvent>>
+                .instance
+                .Subscribe(new EventChannelPublisher(this), handler);
+        }
+
+        public void Unsubscribe<TEvent>(EventHandler<TEvent> handler)
+        {
+            Singleton<EventBus<EventChannelPublisher, TEvent>>
+                .instance
+                .Unsubscribe(new EventChannelPublisher(this), handler);
+        }
+        
+        public void Publish<TEvent>(TEvent e)
+        {
+            Singleton<EventBus<EventChannelPublisher, TEvent>>
+                .instance
+                .Publish(new EventChannelPublisher(this), e);
+        }
+        
+        public void Subscribe<TEvent>(Object publisher, EventHandler<TEvent> handler)
+        {
+            Singleton<EventBus<EventChannelPublisher, TEvent>>
+                .instance
+                .Subscribe(new EventChannelPublisher(this, publisher), handler);
+        }
+
+        public void Unsubscribe<TEvent>(Object publisher, EventHandler<TEvent> handler)
+        {
+            Singleton<EventBus<EventChannelPublisher, TEvent>>
+                .instance
+                .Unsubscribe(new EventChannelPublisher(this, publisher), handler);
+        }
+        
+        public void Publish<TEvent>(Object publisher, TEvent e)
+        {
+            Singleton<EventBus<EventChannelPublisher, TEvent>>
+                .instance
+                .Publish(new EventChannelPublisher(this, publisher), e);
+        }
+    }
+}
---
+++ Runtime/EventBus/EventChannelPublisher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using JetBrains.Annotations;
+using Object = UnityEngine.Object;
+
+namespace Endfield.EventSystem
+{
+    public class EventChannelPublisher : IEquatable<EventChannelPublisher>
+    {
+        public EventChannel channel { get; }
+
+        [CanBeNull]
+        public Object publisher { get; }
+
+        public EventChannelPublisher(EventChannel channel, Object publisher = null)
+        {
+            this.publisher = publisher;
+            this.channel = channel;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is EventChannelPublisher other && Equals(other);
+        }
+
+        public bool Equals(EventChannelPublisher other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            return Equals(channel, other.channel) && Equals(publisher, other.publisher);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(channel, publisher);
+        }
+    }
+}
