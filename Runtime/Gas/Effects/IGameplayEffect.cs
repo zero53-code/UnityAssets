0dfe6b045154815087f81b64e284b3ec8ec214e6
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-05-27 18:01:37 +0800

    添加Apply方法文档注释

--- Runtime/Gas/Effects/IGameplayEffect.cs
+++ Runtime/Gas/Effects/IGameplayEffect.cs
@@ -4,6 +4,11 @@ namespace Zero53.Gas.Effects
 {
     public interface IGameplayEffect
     {
+        /// <summary>
+        /// 应用效果 <para/>
+        /// 会在每帧执行, 直到效果被移除
+        /// </summary>
+        /// <param name="target">目标的属性集</param>
         void Apply(GameplayAttributeSet target);
     }
 }
