9a756a180eebb0429eb2553c0de8c5dbd22146c8
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-05-27 18:02:21 +0800

    新增即时效果抽象类InstantEffect

---
+++ Runtime/Gas/Effects/InstantEffect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Zero53.Gas.Attributes;
+
+namespace Zero53.Gas.Effects
+{
+    /// <summary>
+    /// 即时效果
+    /// </summary>
+    [Serializable]
+    public abstract class InstantEffect : IGameplayEffect
+    {
+        public void Apply(GameplayAttributeSet target)
+        {
+            OnApply(target);
+            target.RemoveEffect(this);
+        }
+        
+        /// <summary>
+        /// 应用效果 <para/>
+        /// 只会执行一次, 然后自动移除效果
+        /// </summary>
+        /// <param name="target">目标的属性集</param>
+        protected abstract void OnApply(GameplayAttributeSet target);
+    }
+}
