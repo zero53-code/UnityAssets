af65bb272527defb93e08ab60078a6ec90390ffc
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-05-28 20:31:01 +0800

    将SOSingleton修改为AssetDatabase实现

--- Runtime/Singletons/SOSingleton.cs
+++ Runtime/Singletons/SOSingleton.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
+using Zero53.Utils;
 
 namespace Zero53.Singletons
 {
+#if UNITY_EDITOR
+    
     /// <summary>
-    /// 基于 Resources 文件夹的 ScriptableObject 单例类
+    /// [Editor-Only] ScriptableObject 单例类
     /// </summary>
     /// <typeparam name="T">单例类型</typeparam>
     public abstract class SOSingleton<T> : ScriptableObject
@@ -14,88 +16,32 @@ namespace Zero53.Singletons
         private static volatile T _instance;
         private static readonly object _lock = new();
 
-#if !UNITY_EDITOR
-        
         public static T instance
         {
             get
             {
-                if (_instance == null)
+                if (_instance != null) return _instance;
+                _instance = FindObjectOfType<T>();
+
+                // 加载项目中所有该类型的 SO
+                var assets = ScriptableObjectUtils.FindAllOnEditor<T>();
+
+
+                if (assets == null || assets.Count == 0)
                 {
-                    lock (_lock)
-                    {
-                        if (_instance == null)
-                        {
-                            _instance = FindObjectOfType<T>();
-
-                            if (_instance == null)
-                            {
-                                // 如果已经存在，直接返回
-                                if (_instance != null)
-                                    return _instance;
-
-                                // 加载项目中所有该类型的SO（自动找到唯一实例）
-                                var assets = Resources.FindObjectsOfTypeAll<T>();
-
-                                if (assets == null || assets.Length == 0)
-                                {
-                                    Debug.LogError($"Singleton of {typeof(T).FullName} could not be found.");
-                                    return null;
-                                }
-
-                                if (assets.Length > 1)
-                                {
-                                    Debug.LogError($"{typeof(T).FullName} already exists.");
-                                    return null;
-                                }
-
-                                _instance = assets[0];
-                                return _instance;
-                            }
-                        }
-                    }
+                    _instance = CreateInstance<T>();
+                    _instance.name = typeof(T).Name;
+                    
+                    AssetDatabase.CreateAsset(_instance, $"Assets/{_instance.name}.asset");
+                    return _instance;
                 }
 
+                _instance = assets[0];
                 return _instance;
+
             }
         }
       
-#else
-        public static T instance
-        {
-            get
-            {
-                var guids = AssetDatabase.FindAssets($"t:{typeof(T).FullName}");
-                var list = new List<T>();
-
-                foreach (var guid in guids)
-                {
-                    var path = AssetDatabase.GUIDToAssetPath(guid);
-                    var so = AssetDatabase.LoadAssetAtPath<T>(path);
-                    list.Add(so);
-                }
-
-                if (list.Count == 0)
-                {
-                    _instance = CreateInstance<T>();
-                    AssetDatabase.CreateAsset(_instance, $"Assets/{typeof(T).Name}.asset");
-                }
-                else
-                {
-                    _instance = list[0];
-
-                    for (var i = 1; i < list.Count; i++)
-                    {
-                        AssetDatabase.DeleteAsset(AssetDatabase.GUIDToAssetPath(guids[i]));
-                    }
-                }
-
-                return _instance;
-            }
-        }
-        
-#endif
-        
         public static bool isInitialized => _instance != null;
         
         protected SOSingleton()
@@ -105,5 +51,24 @@ namespace Zero53.Singletons
                 Debug.LogError($"Singleton of {typeof(T).FullName} already exists.");
             }
         }
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T; 
+                return;
+            }
+
+            AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(this));
+
+            OnAwake();
+        }
+
+        protected virtual void OnAwake()
+        {
+        }
     }
+    
+#endif
 }
