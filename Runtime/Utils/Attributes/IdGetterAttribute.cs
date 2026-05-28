a0490b60671664c6c10f9a4fc83b8ebe4230622b
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-05-28 20:31:24 +0800

    新增特性IdGetterAttribute

---
+++ Runtime/Utils/Attributes/IdGetterAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zero53.Utils.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public sealed class IdGetterAttribute : Attribute
+    {
+        public string name;
+        public IdGetterAttribute(string name = "id")
+        {
+            this.name = name;
+        }
+    }
+}
