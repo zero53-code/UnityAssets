1cdabbd38f39ffbd29ee5e0d20c2c83993832d22
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-05-29 19:15:45 +0800

    新增特性 GameplayTagAttribute

---
+++ Runtime/GameplayTags/Attributes/GameplayTagAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zero53.GameplayTags.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class GameplayTagAttribute  : Attribute
+    {
+        public string[] tags;
+
+        public GameplayTagAttribute(params string[] tags)
+        {
+            this.tags = tags;
+        }
+
+        public GameplayTagAttribute(string tag) : this(new[] { tag })
+        {
+        }
+    }
+}
