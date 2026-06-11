a5177e0cc0e15c23b22f8a8fe0e3706a897a57dc
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-06-11 18:23:26 +0800

    添加新技能触发器

---
+++ Runtime/Gas/GameplayAbilityTriggers/TagActivateGameplayAbilityTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using Zero53.GameplayTags;
+
+namespace Zero53.Gas.GameplayTriggers
+{
+    [Serializable]
+    [Description(description: "基于 tag 触发技能")]
+    public class TagActivateGameplayAbilityTrigger : GameplayAbilityTrigger
+    {
+        /// <summary>
+        /// 激活技能的标签
+        /// </summary>
+        public Tag[] activateAbilityTags;
+        
+        protected internal override void OnUpdate(float deltaTime)
+        {
+            if (abilitySystem.tags.HasAny(activateAbilityTags))
+            {
+                ActivateAbility();
+            }
+        }
+    }
+}
---
+++ Runtime/Gas/GameplayAbilityTriggers/TagDeactivateGameplayAbilityTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using Zero53.GameplayTags;
+
+namespace Zero53.Gas.GameplayTriggers
+{
+    [Serializable]
+    [Description(description: "基于 tag 禁用技能")]
+    public class TagDeactivateGameplayAbilityTrigger : GameplayAbilityTrigger
+    {
+        /// <summary>
+        /// 禁用技能的标签
+        /// </summary>
+        public Tag[] deactivateAbilityTags;
+        
+        protected internal override void OnUpdate(float deltaTime)
+        {
+            if (!abilitySystem.tags.HasAny(deactivateAbilityTags))
+            {
+                ActivateAbility();
+            }
+        }
+    }
+}
