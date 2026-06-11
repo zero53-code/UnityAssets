dd0b58fabdf27aad6e215d866a7cad99f4e8a46c
Zero53 <10163775+maothon@user.noreply.gitee.com>
2026-06-11 13:19:14 +0800

    修改IdGetterAttribute

--- Runtime/Utils/Attributes/IdGetterAttribute.cs
+++ Runtime/Utils/Attributes/IdGetterAttribute.cs
@@ -5,7 +5,7 @@ namespace Zero53.Utils.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     public sealed class IdGetterAttribute : Attribute
     {
-        public string name;
+        public readonly string name;
         public IdGetterAttribute(string name = "id")
         {
             this.name = name;
